8c97a06b8a48f0823e288fb6d1007a2f06244794
telegramabusereportdesk-debug <telegramabusereportdesk@gmail.com>
2026-03-31 10:53:06 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     MONGO_DB_URL = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-    SUPPORT_CHAT = "https://t.me/+WITJGdIyT2kzOGI1"
+    SUPPORT_CHAT = "+WITJGdIyT2kzOGI1"
     UPDATE_CHANNEL = "TheTechXDev"
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp"
     DEV_USERS = [6211784722]
